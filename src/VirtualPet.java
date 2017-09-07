38e2bd2c41fa92a02a86df71b7e118ad9c3759f7
Todd Atkins <toddatkins01@gmail.com>
2017-09-07 19:38:35 -0400

    getting there

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -2,16 +2,34 @@
 public class VirtualPet {
 
 	String petName;
-	int hunger = 5;
+	int hunger = 100;
+	int thirst = 100;
+	int waste = 100;
+	int boredom = 100;
 
-	// thirst
-
-	// waste
-
-	// boredom
-	void food(double amount) {
-
-		hunger += amount;
+	void food() {
+		hunger += 10;
+		// thirst -= 5;
+		// waste += 15;
+		// boredom -= 10;
 	}
 
+	void liquid(double amount) {
+		thirst += amount;
+	}
+
+	void potty(double amount) {
+		waste += amount;
+	}
+
+	void play(double amount) {
+		boredom += amount;
+	}
+
+	void tick() {
+		hunger -= 2;
+		thirst += 2;
+		waste += 2;
+		boredom += 2;
+	}
 }
