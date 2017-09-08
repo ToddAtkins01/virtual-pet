c0bad44dc92b4fa478b7dd67776e4f6c760eff24
Todd Atkins <toddatkins01@gmail.com>
2017-09-07 20:28:24 -0400

    done bare min

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -2,28 +2,45 @@
 public class VirtualPet {
 
 	String petName;
+
 	int hunger = 100;
 	int thirst = 100;
 	int waste = 100;
 	int boredom = 100;
 
+	boolean petIsAlive(String petName) {
+		if (hunger > 0 && thirst > 0 && waste < 150 && boredom > 0) {
+			return true;
+		}
+		return false;
+	}
+
 	void food() {
 		hunger += 10;
-		// thirst -= 5;
-		// waste += 15;
-		// boredom -= 10;
+		thirst -= 5;
+		waste += 15;
+		boredom += 10;
 	}
 
-	void liquid(double amount) {
-		thirst += amount;
+	void liquid() {
+		hunger += 0;
+		thirst += 10;
+		waste += 10;
+		boredom += 5;
 	}
 
-	void potty(double amount) {
-		waste += amount;
+	void potty() {
+		hunger += 0;
+		thirst += 0;
+		waste -= 20;
+		boredom += 5;
 	}
 
-	void play(double amount) {
-		boredom += amount;
+	void play() {
+		hunger += 15;
+		thirst += 20;
+		waste += 1;
+		boredom -= 20;
 	}
 
 	void tick() {
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -11,50 +11,57 @@ public class VirtualPetApp {
 		System.out.println("Welcome to this AWESOME Virtual Pet App!! \nPlease name your pet.");
 		digiPet.petName = input.next();
 
-		int userOpt;
+		while (digiPet.petIsAlive(digiPet.petName)) {
+			int userOpt;
 
-		do {
-			System.out.println("What do you want to do? \n");
-			System.out.println("Press 1 to Feed " + digiPet.petName);
-			System.out.println("Press 2 to Give " + digiPet.petName + " liuid");
-			System.out.println("Press 3 to Take " + digiPet.petName + " to the potty");
-			System.out.println("Press 4 to Play with " + digiPet.petName);
-			System.out.println("Press 5 to Do nothing with " + digiPet.petName);
-			userOpt = input.nextInt();
+			do {
+				System.out.println("What do you want to do? \n");
+				System.out.println("Press 1 to Feed " + digiPet.petName);
+				System.out.println("Press 2 to Give " + digiPet.petName + " liuid");
+				System.out.println("Press 3 to Take " + digiPet.petName + " to the potty");
+				System.out.println("Press 4 to Play with " + digiPet.petName);
+				System.out.println("Press 5 to Do nothing with " + digiPet.petName);
+				userOpt = input.nextInt();
 
-		} while (userOpt != 1 && userOpt != 2 && userOpt != 3 && userOpt != 4 && userOpt != 5);
+			} while (userOpt != 1 && userOpt != 2 && userOpt != 3 && userOpt != 4 && userOpt != 5);
 
-		switch (userOpt) {
+			switch (userOpt) {
 
-		case 1:
-			System.out.println(digiPet.petName + " thanks you for that tasty morsal!");
-			digiPet.food();
-			digiPet.tick();
-			System.out.println("\n" + digiPet.petName + "'s hunger is now at: " + (digiPet.hunger) + "\n");
-			break;
+			case 1:
+				System.out.println(digiPet.petName + " thanks you for that tasty morsal!");
+				digiPet.food();
+				digiPet.tick();
+				System.out.println("\n" + digiPet.petName + "'s hunger is now at: " + (digiPet.hunger) + "\n");
+				break;
 
-		// case 2:
-		// System.out.println("Please enter the amount you would like to withdraw.");
-		// double enteredWithdraw = input.nextDouble();
-		// bankOfMe.withdraw(enteredWithdraw);
-		// System.out.println("Your new balance is: " +
-		// curancyFormat.format(bankOfMe.balance) + "\n");
-		// break;
-		//
-		// case 3:
-		// System.out.println("Your balance is: " +
-		// curancyFormat.format(bankOfMe.balance) + "\n");
-		// break;
-		//
-		// case 4:
-		// System.out.println("Thank you for being a part of Bank Of Me.\nHave a great
-		// day!");
-		// System.exit(0);
-		//
-		// }
-		//
+			case 2:
+				System.out.println(digiPet.petName + " Says, Finally! I was dieing of thirst!");
+				digiPet.liquid();
+				digiPet.tick();
+				System.out.println("\n" + digiPet.petName + "'s thirst is now at: " + (digiPet.thirst) + "\n");
+				break;
+
+			case 3:
+				System.out.println(digiPet.petName + " Says, THANK YOU! THANK YOU! I thought I was going to explode!");
+				digiPet.potty();
+				digiPet.tick();
+				System.out.println("\n" + digiPet.petName + "'s bladder is now at: " + (digiPet.waste) + "\n");
+				break;
+
+			case 4:
+				System.out.println(digiPet.petName + " Says, Woo Hoo!! We get to play!");
+				digiPet.play();
+				digiPet.tick();
+				System.out.println("\n" + digiPet.petName + "'s bladder is now at: " + (digiPet.waste) + "\n");
+				break;
+
+			}
+			if (!digiPet.petIsAlive(digiPet.petName)) {
+				System.out.println("You KILLED " + digiPet.petName + "! You BASTARD!!!!");
+
+			}
+
 		}
-		//
 		input.close();
 	}
 
